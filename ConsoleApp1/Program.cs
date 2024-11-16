9787cf6c75d2b3e63ca59735528551fe7cb46f95
Georgios Kalanidis <georgios.kalanidis@elev.ga.ntig.se>
2024-11-16 19:11:42 +0100

    klar

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,143 @@
+﻿//string[] names = ["Bolar", "Lego", "Playmobil", "Slime", "Hot Wheels"];
+
+//Random random = new Random(); 
+//int randomIndex = random.Next(names.Length); 
+
+//Console.WriteLine(names[randomIndex]); 
+//Console.ReadLine();
+
+
+
+
+
+//string[] choices = {"Bolar", "Lego", "Playmobil", "Slime", "Hot Wheels"};
+
+//foreach (string choice in choices)
+//{
+  //Console.WriteLine(choice);
+//}
+//Console.ReadLine();
+
+
+
+
+
+//string[] names = ["Damian", "Wing", "Romeo", "Duck", "Sador"];
+
+//Random random = new Random(); 
+//int randomIndex = random.Next(names.Length); 
+
+//Console.WriteLine(names[randomIndex]); 
+//Console.ReadLine();
+
+
+
+
+
+
+//int[] numbers = { 3, 7, 2, 10, 5 };
+
+//foreach (int number in numbers)
+//{
+    //Console.WriteLine(number);
+//}
+//Console.ReadLine();
+
+
+
+
+
+
+
+
+
+
+
+
+//class Program
+//{
+   // static void Main()
+    //{
+      //  string[] classmates = { "Wing", "Sador", "Duck", "Romeo", "Damian" };
+       // string[] toys = { "Hotwheels", "Slime", "Bolar", "Playmobil", "Lego" };
+       // int[] ratings = { 3, 7, 2, 10, 5 };
+
+        //for (int i = 0; i < classmates.Length; i++) // Iterera genom alla element
+        //{
+         //   Console.WriteLine($"{classmates[i]} ger {toys[i]} betyget {ratings[i]}");
+        //}
+
+        //Console.ReadLine();
+   // }
+//}
+
+
+
+
+
+
+//class Program
+//{
+   // static void Main()
+   // {
+        //List<string> cities = new List<string>();
+
+       // Console.WriteLine("Listan 'cities' är tom.");
+       // Console.ReadLine();
+    //}
+//}
+
+
+
+
+
+
+
+
+
+
+
+//class Program
+
+//{
+   // static void Main()
+  //  {
+       // List<string> cities = new List<string>();
+       // string input;
+
+       // Console.WriteLine("Skriv in städer för att lägga till i listan. Skriv 'exit' för att avsluta.");
+
+       // while (true)
+       // {
+           // Console.Write("Ange en stad: ");
+           // input = Console.ReadLine();
+
+            //if (input.ToLower() == "exit")
+            //{
+              //  break; 
+           // }
+
+            //cities.Add(input); 
+       //     Console.WriteLine($"'{input}' har lagts till i listan.");
+      // }
+
+       // Console.WriteLine("\nDu har lagt till följande städer:");
+        //foreach (string city in cities)
+        //{
+        //    Console.WriteLine(city);
+ //       }
+
+ //      Console.ReadLine();
+ //   }
+//}
+
+
+
+
+
+
+
+
+
+
+
